5b86a2c82a260aca9c1534874d65a5740cb30b71
Adrian Maenzanise <adrian@slickdesigns4u.com>
2018-06-30 20:11:49 +0200

    Initial commit

---
+++ src/components/pages/Home.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>You are on the home page.</h1>
+</template>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,28 @@
+import '@babel/polyfill'
 import Vue from 'vue'
+import './plugins/vuetify'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+
+
+// Pages
+import home from './components/pages/Home.vue'
+import about from './components/pages/About.vue'
+import contact from './components/pages/Contact.vue'
 
 Vue.config.productionTip = false
+Vue.use(VueRouter)
 
+const routes = [
+  {path : '/', component: home},
+  {path : '/about', component: about},
+  {path : '/contact', component: contact},
+]
+
+const router = new VueRouter({
+  routes
+})
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
